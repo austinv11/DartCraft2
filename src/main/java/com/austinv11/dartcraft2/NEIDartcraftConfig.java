87cac75a905019c901ee32ac93f62bd3e3a009f9
austinv11 <austinv11@gmail.com>
2015-04-08 17:35:53 -0400

    Added NEI support to the clipboard

---
+++ src/main/java/com/austinv11/dartcraft2/NEIDartcraftConfig.java
@@ -0,0 +1,26 @@
+package com.austinv11.dartcraft2;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import codechicken.nei.recipe.DefaultOverlayHandler;
+import com.austinv11.dartcraft2.client.gui.GuiClipboard;
+import com.austinv11.dartcraft2.reference.Reference;
+
+public class NEIDartcraftConfig implements IConfigureNEI {
+	
+	@Override
+	public void loadConfig() {
+		API.registerGuiOverlay(GuiClipboard.class, "crafting");
+		API.registerGuiOverlayHandler(GuiClipboard.class, new DefaultOverlayHandler(), "crafting");
+	}
+	
+	@Override
+	public String getName() {
+		return Reference.MOD_NAME+" NEI Plugin";
+	}
+	
+	@Override
+	public String getVersion() {
+		return Reference.VERSION;
+	}
+}
