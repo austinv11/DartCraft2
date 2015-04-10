b5a71fef9d2986414a65a519ea12871a847d52dc
austinv11 <austinv11@gmail.com>
2015-04-10 17:57:51 -0400

    I just learned about the magic of the @API annotation
    Enjoy prospective addon devs!

---
+++ src/main/java/com/austinv11/dartcraft2/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = "DartCraft2", provides = "DartCraft2|API", apiVersion = Reference.VERSION)
+package com.austinv11.dartcraft2.api;
+
+import com.austinv11.dartcraft2.reference.Reference;
+import cpw.mods.fml.common.API;
